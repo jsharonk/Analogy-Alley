0f1d1c5001960e7583615ebd748d2880cc96148d
jsk <jsharonkim@gmail.com>
2017-01-18 16:40:00 -0500

    omitted match[4] and port

--- db/models.js
+++ db/models.js
@@ -13,8 +13,8 @@ if (!global.hasOwnProperty('db')) {
     sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_RED_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
+      // port:     match[4],
+      // host:     match[3],
       logging:  true //false
     })
   } else {
