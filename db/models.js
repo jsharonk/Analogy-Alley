1b3030d54cd41051e943c1fd00e94350510dd5c2
jsk <jsharonkim@gmail.com>
2017-01-18 16:23:37 -0500

    postgres cubic

--- db/models.js
+++ db/models.js
@@ -4,6 +4,23 @@ const Sequelize = require('sequelize');
 const pg = require('pg');
 // const DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
 
+if (process.env.postgresql-cubic-42207) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.postgresql-cubic-42207, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  // sequelize = new Sequelize("postgres:///my_db");
+
+ const db = new Sequelize(
+  'postgres://localhost:5432/analogies', {
+    logging: false
+  }
+);
+}
 
 //  const db = new Sequelize(
 //   'postgres://localhost:5432/analogies', {
@@ -13,9 +30,9 @@ const pg = require('pg');
 
 // console.log('process', process.env.DATABASE_URL);
 
-const db = process.env.DATABASE_URL || 3000
-client = new pg.Client db
-client.connect()
+// const db = process.env.DATABASE_URL || 3000
+// client = new pg.Client db
+// client.connect()
 
 const Analogy = db.define('analogy', {
   name: {
