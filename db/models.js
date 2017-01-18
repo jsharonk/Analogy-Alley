00dda4ce2a8f0be1216e5e801623a329300ca3f7
jsk <jsharonkim@gmail.com>
2017-01-18 16:11:38 -0500

    passing db string

--- db/models.js
+++ db/models.js
@@ -1,17 +1,21 @@
 'use strict';
 
 const Sequelize = require('sequelize');
+const pg = require('pg');
 // const DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
 
 
- const db = new Sequelize(
-  'postgres://localhost:5432/analogies', {
-    logging: false
-  }
-);
+//  const db = new Sequelize(
+//   'postgres://localhost:5432/analogies', {
+//     logging: false
+//   }
+// );
 
 // console.log('process', process.env.DATABASE_URL);
 
+const db = process.env.DATABASE_URL or LOCAL_DB
+client = new pg.Client db
+client.connect()
 
 const Analogy = db.define('analogy', {
   name: {
