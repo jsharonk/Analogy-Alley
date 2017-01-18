923756212d289ff1e7542fddc4d22a1df16d136e
jsk <jsharonkim@gmail.com>
2017-01-18 15:52:20 -0500

    process env database url

--- db/models.js
+++ db/models.js
@@ -2,28 +2,28 @@
 
 const Sequelize = require('sequelize');
 
-// if (process.env.DATABASE_URL) {
-//   // the application is executed on Heroku ... use the postgres database
-//   const db= new Sequelize('postgres://yhtjhkxdmlkzve:1a0ee89930c458502f1c5996812ce2dd96a9486d189e1310b19833ab332007a0@ec2-54-243-185-99.compute-1.amazonaws.com:5432/dboh53q03qs7f8'
-// , {
-//     dialect:  'postgres',
-//     protocol: 'postgres',
-//     logging:  true //false
-//   });
-// } else {
-//   // the application is executed on the local machine
-//   // sequelize = new Sequelize("postgres:///my_db");
-//   const db = new Sequelize(
-//   'postgres://localhost:5432/analogies', {
-//     logging: false
-//   }
-// );
-// }
- const db = new Sequelize(
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  const db= new Sequelize('postgres://yhtjhkxdmlkzve:1a0ee89930c458502f1c5996812ce2dd96a9486d189e1310b19833ab332007a0@ec2-54-243-185-99.compute-1.amazonaws.com:5432/dboh53q03qs7f8'
+, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  // sequelize = new Sequelize("postgres:///my_db");
+  const db = new Sequelize(
   'postgres://localhost:5432/analogies', {
     logging: false
   }
 );
+}
+//  const db = new Sequelize(
+//   'postgres://localhost:5432/analogies', {
+//     logging: false
+//   }
+// );
 
 // console.log('process', process.env.DATABASE_URL);
 
