144809262de8f54582303ea1e9752b17b906d6d0
jsk <jsharonkim@gmail.com>
2017-01-18 13:24:18 -0500

    postgres

--- db/models.js
+++ db/models.js
@@ -2,19 +2,22 @@
 
 const Sequelize = require('sequelize');
 
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
-    protocol: 'postgres',
-    logging:  true //false
-  });
-} 
- const db = new Sequelize(
-  'postgres://localhost:5432/analogies', {
-    logging: false
-  }
-);
+// if (process.env.DATABASE_URL) {
+//   // the application is executed on Heroku ... use the postgres database
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect:  'postgres',
+//     protocol: 'postgres',
+//     logging:  true //false
+//   });
+// } 
+
+    // var client = new Client(process.env.DATABASE_URL); //something like this should get you running with heroku
+const db = new Sequelize(process.env.DATABASE_URL);
+//  const db = new Sequelize(
+//   'postgres://localhost:5432/analogies', {
+//     logging: false
+//   }
+// );
 
 const Analogy = db.define('analogy', {
   name: {
